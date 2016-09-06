023a20a8323db8b7e60736ca98fe70f38208e4de
luben <luben93@gmail.com>
2016-09-06 22:26:41 +0200

    init

---
+++ autoinstall.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# post install for arch linux + awesome wm desktop, requires git
+rooted (){
+    pacman -Syu
+    pacman -S sudo git vim zsh xorg-server awesome termite tmux base-devel xterm xorg-xinit --noconfirm
+    echo '[archlinuxfr]
+    SigLevel = Never
+    Server = http://repo.archlinux.fr/$arch' >> /etc/pacman.conf
+    pacman -Sy yaourt --noconfirm
+}
+
+headless() {
+    yaourt -S --noconfirm dex htop-temperature atop cmatrix cowsay fortune dialog openssh iw curl \
+    linux-headers ntfs-3g unzip transmission-cli wpa_supplicant hfsprogs  atop cmatrix cowsay fortune dialog openssh 
+}
+
+graphical(){
+    yaourt -S --noconfirm xbindkeys spotify google-chrome intellij-idea-ultimate-edition playerctl  \
+    mopidy-mpris mopidy-spotify mopidy-mopify lain-git gloobus-preview redshift onboard openjdk8    \
+    lightdm-gtk-greeter lightdm mpc pulseaudio pulseaudio-alsa pulseaudio-ctl screenfetch vlc       \
+    transmission-qt
+}
+
+conf(){
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
+    curl http://j.mp/spf13-vim3 -L -o - | sh
+    mkdir -P ~/.config
+    git clone https://github.com/luben93/awesome-copycats ~/.config/awesome
+    ln ~/.config/awesome/rc.lua.powerarrow-darker.luben ~/.config/awesome/rc.lua 
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    echo " set-option -g mouse on
+    set -g @continuum-restore 'on'
+    
+    # List of plugins
+    set -g @plugin 'tmux-plugins/tpm'
+    set -g @plugin 'tmux-plugins/tmux-sensible'
+    set -g @plugin 'nhdaly/tmux-scroll-copy-mode'
+    set -g @plugin 'tmux-plugins/tmux-resurrect'
+    set -g @plugin 'tmux-plugins/tmux-continuum'
+    
+    run '~/.tmux/plugins/tpm/tpm'" >> ~/.tmux.conf 
+    ~/.tmux/plugins/tpm/bindings/install_plugins 
+
+}
+
+
