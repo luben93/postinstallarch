7395069d6b0e189ecc7e08ba48e39f3eefe55675
luben <luben93@gmail.com>
2016-09-06 23:43:32 +0200

    fixing headless

--- rooted.sh
+++ rooted.sh
@@ -4,15 +4,17 @@ rooted (){
     pacman -Syu
     pacman -S sudo git vim zsh  tmux base-devel  --noconfirm
     pacman -S --noconfirm xorg-server awesome termite xterm xorg-xinit # graphical 
-        echo '[archlinuxfr]
-    SigLevel = Never
-    Server = http://repo.archlinux.fr/$arch' >> /etc/pacman.conf
+    if ! grep archlinuxfr /etc/pacman.conf 
+    then echo '[archlinuxfr]
+        SigLevel = Never
+        Server = http://repo.archlinux.fr/$arch' >> /etc/pacman.conf
+    fi
     pacman -Sy yaourt --noconfirm
 }
 
 headless() {
     yaourt -S --noconfirm dex htop-temperature atop cmatrix cowsay fortune dialog openssh iw curl \
-    linux-headers ntfs-3g unzip transmission-cli wpa_supplicant hfsprogs  atop cmatrix cowsay dialog openssh 
+        linux-headers ntfs-3g unzip transmission-cli wpa_supplicant hfsprogs  atop cmatrix cowsay dialog openssh 
 }
 
 rooted 
