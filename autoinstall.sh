f3e9b91a950f6131baa0bf441e76f35214fda815
lucas <luben93@users.noreply.github.com>
2018-12-13 09:39:17 +0100

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -6,7 +6,7 @@ graphical (){
     yaourt -S --noconfirm spotify firefox intellij-idea-community-edition  \
     gloobus-preview redshift onboard  cool-retro-term gendesk xclip playerctl light-locker lightdm-gtk-greeter lightdm \
     pulseaudio pulseaudio-alsa pulseaudio-ctl screenfetch vlc transmission-qt alsa-utils pavucontrol nitrogen \
-    thunderbird arandr lastpass qemu libvirt ovmf virt-manager arandr slack-desktop darktable gdb calibre eog evince
+    thunderbird arandr lastpass qemu libvirt ovmf virt-manager arandr slack-desktop darktable gdb calibre eog #evince-no-gnome
     touch grap  
 }
 
